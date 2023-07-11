c919327dec30a493abebd0c286854907771c8f36
santosh <santosh777786@gmail.com>
2023-07-11 09:06:12 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeeController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+
+            var employees = await mvcDemoDbContext.Employees.ToListAsync();
+            return View(employees);
+
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+     
+
+        [HttpPost]
+        public  async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth
+
+            };
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task< IActionResult >View(Guid id) 
+        {
+            var employee = await mvcDemoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee != null)
+            {
+                var viewModel = new Update()
+                {
+
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth
+                };
+                return await Task.Run(()=>View("View",viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult >View(Update model)
+        {
+            var employee =await mvcDemoDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                employee.Id = model.Id;
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+
+                await mvcDemoDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Add");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Remove(Update model) 
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+            if (employee != null) 
+            {
+                mvcDemoDbContext.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ Models/Domain/Update.cs
@@ -0,0 +1,14 @@
+﻿namespace ASPNETMVCCRUD.Models.Domain
+
+{
+    public class Update
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+        
+    }
+}
